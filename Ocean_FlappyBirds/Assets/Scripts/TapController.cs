0cad53a60847574a24d1bf018e5eaaae63460d25
NicksonK <nickson.kley@gmail.com>
2020-07-08 09:24:26 -0400

    uploads de projeto somente com os assets

---
+++ Ocean_FlappyBirds/Assets/Scripts/TapController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class TapController : MonoBehaviour
+{
+    public float force = 150;
+    public Vector3 startPosition;
+
+    Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rig = GetComponent<Rigidbody2D>();
+        rig.transform.position = startPosition;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            rig.AddForce(Vector2.up * force, ForceMode2D.Force);
+
+        }
+        
+    }
+}
